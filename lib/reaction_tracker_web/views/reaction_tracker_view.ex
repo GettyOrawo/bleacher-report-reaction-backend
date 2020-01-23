42207a5895447e77ab5dda485e78b3569f000c4f
GettyOrawo <gettyorawo@gmail.com>
2020-01-23 18:54:23 +0300

    add views to be rendered

---
+++ lib/reaction_tracker_web/views/reaction_tracker_view.ex
@@ -0,0 +1,19 @@
+defmodule ReactionTrackerWeb.TrackerView do
+
+  use ReactionTrackerWeb, :view
+
+  def render("tracker.json", %{tracker: tracker}) do
+    %{
+      type: tracker.type,
+      action: tracker.action,
+      content_id: tracker.content_id,
+      user_id: tracker.user_id,
+      reaction_type: tracker.reaction_type
+    }
+  end
+
+  def render("index.json", %{trackers: trackers}) do
+    %{data: render_many(trackers, ReactionTrackerWeb.TrackerView, "tracker.json")}
+  end
+
+end
