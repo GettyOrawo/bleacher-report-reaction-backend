498233b050319ecabc972caf4be4456aeec48c8f
GettyOrawo <gettyorawo@gmail.com>
2020-01-23 18:15:17 +0300

    Add table fields from the provided payload

--- priv/repo/migrations/20200123133422_create_tracker.exs
+++ priv/repo/migrations/20200123133422_create_tracker.exs
@@ -2,6 +2,14 @@ defmodule ReactionTracker.Repo.Migrations.CreateTracker do
   use Ecto.Migration
 
   def change do
+    create_table(:reaction_tracker, primary_key: false) do
+      add: :type, :string
+      add: :action, :string
+      add: :content_id, :string
+      add: :user_id, :string
+      add: :reaction_type, :string
 
+      timestamps()
+    end
   end
 end
