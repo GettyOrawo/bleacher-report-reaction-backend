c7349442c5aa038e956c0c28f0bc5264e91f1950
GettyOrawo <gettyorawo@gmail.com>
2020-01-23 18:28:07 +0300

    create a model with schema and changeset

---
+++ lib/reaction_tracker/tracker.ex
@@ -0,0 +1,23 @@
+defmodule ReactionTracker.Tracker do
+
+  use ReactionTracker.Web, :model
+
+  schema "reaction_tracker" do
+    field :type, :string
+    field :action, :string
+    field :content_id, :string
+    field :user_id, :string
+    field :reaction_type, :string
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the Struct and Params
+  """
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:type,:action, :content_id, :user_id, :reaction_type])
+    |> validate_required([type,:action, :content_id, :user_id, :reaction_type])
+  end
+end
